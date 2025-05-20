038875f1cb27ed88771066f2607990187b5856c0
gonzalomartinez520 <gonzalomartinez520@gmail.com>
2025-05-19 21:17:15 -0300

    Delete views directory

--- views/views/scripts/eliminarNoticia.js
+++
@@ -1,9 +0,0 @@
-function eliminarNoticia(noticia) {
-    let noticias = JSON.parse(localStorage.getItem('noticias')) || [];
-    noticias = noticias.filter(n => n.fecha != noticia.fecha);
-
-    localStorage.setItem('noticias', JSON.stringify(noticias));
-
-    document.getElementById('contenedor-noticias').innerHTML = '';
-    cargarNoticiasParaAdmin();
-}
